8029b7dd3a6f143397eae99320d554e3dab7abed
hundredvisionsguy <cwinikka@gmail.com>
2017-04-21 09:12:46 -0700

    fixing test case error

--- DistanceBetweenTwoPointsCalculator.py
+++ DistanceBetweenTwoPointsCalculator.py
@@ -2,12 +2,11 @@
 # Your job is to write a function in DistanceBetweenPointsCalculator.py (call
 # it **calculateDistanceBetween()** that calculates the distance between two points
 # in 2D space (x1, y1) and (x2, y2)
-# factor based on The Distance Formula
+# based on The Distance Formula
 # mathwarehouse.com (http://www.mathwarehouse.com/algebra/distance_formula/index.php)
 
 # Define Function below
 # be sure to return an integer
-
 
 if __name__ == '__main__':
     # Call the function in here if you want to test it
